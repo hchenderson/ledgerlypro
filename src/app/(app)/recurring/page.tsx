bfe84b8c2f09eaf29da9499df7514e50dcb02c09
Carter Henderson <hendersonranch3@gmail.com>
2025-09-08 15:04:45 +0000

    Looking at this error, there's a **syntax error in your React/Next.js co

--- src/app/(app)/recurring/page.tsx
+++ src/app/(app)/recurring/page.tsx
@@ -123,16 +123,22 @@ function RecurringDialog({ transaction, onSave, children }: { transaction?: Recu
                 </Select><FormMessage /></FormItem>
             )}/>
             <FormField control={form.control} name="startDate" render={({ field }) => (
-                <FormItem className="flex flex-col"><FormLabel>Start Date</FormLabel>
-                    <Popover><PopoverTrigger asChild><FormControl>
-                        <Button variant={"outline"} className={cn("pl-3 text-left font-normal", !field.value && "text-muted-foreground")}>
-                          {field.value ? format(field.value, "PPP") : (<span>Pick a date</span>)}
-                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                        </Button>
-                    </FormControl></PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="start">
-                        <Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus/>
-                    </PopoverContent></Popover><FormMessage />
+                <FormItem className="flex flex-col">
+                    <FormLabel>Start Date</FormLabel>
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <FormControl>
+                                <Button variant={"outline"} className={cn("pl-3 text-left font-normal", !field.value && "text-muted-foreground")}>
+                                  {field.value ? format(field.value, "PPP") : (<span>Pick a date</span>)}
+                                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                            </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-auto p-0" align="start">
+                            <Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus/>
+                        </PopoverContent>
+                    </Popover>
+                    <FormMessage />
                 </FormItem>
             )}/>
             <DialogFooter>
