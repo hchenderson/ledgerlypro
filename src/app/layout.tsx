08b74175ba46e193239108830aae9b298687fc7a
Carter Henderson <hendersonranch3@gmail.com>
2026-01-17 03:21:18 +0000

    i am logged in when asking Ledgerly Assistant a question. However the re

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,10 +32,10 @@ export default function RootLayout({
               disableTransitionOnChange
             >
               {children}
+              <Toaster />
+              <ChatFab />
             </ThemeProvider>
           </AuthProvider>
-        <Toaster />
-        <ChatFab />
       </body>
     </html>
   );
