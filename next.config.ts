bb8d1f02dc4413213f6d116238db89fe96b9e0e4
Carter Henderson <hendersonranch3@gmail.com>
2026-01-19 16:25:32 +0000

    Error: ./.env.local: line 19: unexpected EOF while looking for matching

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 
 import type {NextConfig} from 'next';
-import withPWA from 'next-pwa';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -41,11 +40,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-const pwaConfig = withPWA({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-});
-
-export default pwaConfig(nextConfig);
+export default nextConfig;
